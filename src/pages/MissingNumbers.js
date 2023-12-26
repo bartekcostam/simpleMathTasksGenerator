30c09c1e000aabf848a2b42ab9b273fe57a48185
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2023-12-26 16:01:47 +0100

    adding multiple lines in missing numbers page

--- src/pages/MissingNumbers.js
+++ src/pages/MissingNumbers.js
@@ -1,45 +1,58 @@
-import React from 'react'
-import {useState} from 'react'
-import './MissingNumbers.css'
-
-
-
+import React, { useState } from 'react';
+import './MissingNumbers.css';
 
 const MissingNumbers = () => {
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(0);
 
-  const [from, setFrom] = useState(0)
-  const [to, setTo] = useState(0)
-  const [generateNumbersArr, setGenerateNumbers] = useState([])
-  
-  const From = (event) =>{
-    setFrom(parseInt(event.target.value),10)
-    }
-    const To = (event) =>{
-      setTo(parseInt(event.target.value),10)
-    }
+  const handleFromChange = (event) => {
+    setFrom(parseInt(event.target.value, 10));
+  };
 
-    const generateNumbers = () => {
-      let arr = []
-      for(let i = from; i < to; i++) {
-        const showNumber = Math.random() < 0.2
-        arr.push(<div key={i} className="inline">{showNumber ? i : ''}</div>)
-      }
-       return setGenerateNumbers(arr)
-    }
+  const handleToChange = (event) => {
+    setTo(parseInt(event.target.value, 10));
+  };
 
+  const generateNumbers = () => {
+    let arr = [];
+    for (let i = from; i <= to; i++) {
+      const showNumber = Math.random() < 0.2;
+      arr.push(<div key={i} className="inline">{showNumber ? i : ''}</div>);
+    }
+    return arr;
+  };
 
   return (
     <div>
-    <div>Generate missing numbers</div>
-      <input  type="number" placeholder="from" onChange={From}/>
-      <input  type="number" placeholder="to" onChange={To}/>
-      <button onClick={generateNumbers}> Generate</button>
-      <hr></hr>
-      {generateNumbersArr}
-
+      <div>Generate missing numbers</div>
+      <input type="number" placeholder="from" onChange={handleFromChange} />
+      <input type="number" placeholder="to" onChange={handleToChange} />
+      <button onClick={generateNumbers}>Generate</button>
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      <hr />
+      {generateNumbers()}
+      
     </div>
+  );
+};
 
-  )
-}
-
-export default MissingNumbers
+export default MissingNumbers;
